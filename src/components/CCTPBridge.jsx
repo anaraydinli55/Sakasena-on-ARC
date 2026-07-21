57bc9927f2075eeef8f1becd8c318d80ad89db51
Anar Aydinli <109851305+anaraydinli55@users.noreply.github.com>
2026-07-21 19:43:22 +0400

    Remove comment regarding depositForBurn function
    Removed commented line about CCTP v2 depositForBurn function.

--- src/components/CCTPBridge.jsx
+++ src/components/CCTPBridge.jsx
@@ -73,7 +73,6 @@ const CCTP_CONTRACTS = {
 };
 
 const TOKEN_MESSENGER_ABI = [
-  // CCTP v2: depositForBurn artik geriye deger dondurmemektedir.
   "function depositForBurn(uint256 amount, uint32 destinationDomain, bytes32 mintRecipient, address burnToken, bytes32 destinationCaller, uint256 maxFee, uint32 minFinalityThreshold) external",
   "event DepositForBurn(address indexed burnToken, uint256 amount, address indexed depositor, bytes32 mintRecipient, uint32 destinationDomain, bytes32 destinationTokenMessenger, bytes32 destinationCaller, uint256 maxFee, uint32 indexed minFinalityThreshold, bytes hookData)",
   "event MessageSent(bytes message)",
